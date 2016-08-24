57036dec284eb68d35830d13fa89848e071395b6
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2016-08-24 16:10:31 +0530

    Add google protobuf serialisation

--- src/googleprotobuf_json/Googleprotobuf_json.java
+++ src/googleprotobuf_json/Googleprotobuf_json.java
@@ -16,7 +16,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
-
+import googleprotobuf_json.ResultProto.Result;
+import googleprotobuf_json.ResultProto.StudentMsg;
+import googleprotobuf_json.ResultProto.CourseMarks;
+import java.io.FileOutputStream;
 
 /**
  *
@@ -52,6 +55,24 @@ public class Googleprotobuf_json {
     /**
      * @param args the command line arguments
      */
+    
+    private static Result getResultProtoFromStudents(Student[] students){
+        Result.Builder result = Result.newBuilder();
+        for(Student student: students) {
+            StudentMsg.Builder studentMsg = StudentMsg.newBuilder();
+            studentMsg.setName(student.Name);
+            studentMsg.setRollNum(student.RollNo);
+            for(Course course: student.CourseMarks) {
+                CourseMarks.Builder courseMarks = CourseMarks.newBuilder();
+                courseMarks.setName(course.CourseName);
+                courseMarks.setScore(course.CourseScore);
+                studentMsg.addMarks(courseMarks);
+            }
+            result.addStudentmsg(studentMsg);
+        }
+        return result.build();
+    }
+    
     private static Student[] parseStringToStudents(List<String> lines) {
         ArrayList<Student> studentsList = new ArrayList<Student>();
         
@@ -101,16 +122,25 @@ public class Googleprotobuf_json {
         
 //        String ss = "[{\"Name\":\"JANICE\",\"CourseMarks\":[{\"CourseScore\":54,\"CourseName\":\"cs125\"},{\"CourseScore\":73,\"CourseName\":\"me420\"},{\"CourseScore\":65,\"CourseName\":\"me119\"},{\"CourseScore\":44,\"CourseName\":\"cs431\"},{\"CourseScore\":67,\"CourseName\":\"cs481\"}],\"RollNo\":202817265},{\"Name\":\"JACK\",\"CourseMarks\":[{\"CourseScore\":42,\"CourseName\":\"ec904\"},{\"CourseScore\":69,\"CourseName\":\"me420\"},{\"CourseScore\":47,\"CourseName\":\"cs481\"},{\"CourseScore\":35,\"CourseName\":\"ec224\"},{\"CourseScore\":82,\"CourseName\":\"cs125\"},{\"CourseScore\":89,\"CourseName\":\"cs431\"},{\"CourseScore\":53,\"CourseName\":\"cs570\"},{\"CourseScore\":90,\"CourseName\":\"me119\"},{\"CourseScore\":40,\"CourseName\":\"cs565\"}],\"RollNo\":202817266},{\"Name\":\"WARREN\",\"CourseMarks\":[{\"CourseScore\":82,\"CourseName\":\"ec904\"}],\"RollNo\":202817267},{\"Name\":\"OSCAR\",\"CourseMarks\":[{\"CourseScore\":55,\"CourseName\":\"cs565\"},{\"CourseScore\":87,\"CourseName\":\"me119\"}],\"RollNo\":202817268},{\"Name\":\"BEATRICE\",\"CourseMarks\":[{\"CourseScore\":65,\"CourseName\":\"cs570\"},{\"CourseScore\":43,\"CourseName\":\"me119\"},{\"CourseScore\":57,\"CourseName\":\"ec224\"},{\"CourseScore\":36,\"CourseName\":\"ci325\"},{\"CourseScore\":33,\"CourseName\":\"cs125\"},{\"CourseScore\":74,\"CourseName\":\"cs431\"},{\"CourseScore\":81,\"CourseName\":\"me420\"},{\"CourseScore\":55,\"CourseName\":\"cs565\"}],\"RollNo\":202817269},{\"Name\":\"JEROME\",\"CourseMarks\":[{\"CourseScore\":32,\"CourseName\":\"ec904\"},{\"CourseScore\":59,\"CourseName\":\"ci325\"},{\"CourseScore\":25,\"CourseName\":\"cs570\"},{\"CourseScore\":58,\"CourseName\":\"cs431\"}],\"RollNo\":202817270},{\"Name\":\"KELLY\",\"CourseMarks\":[{\"CourseScore\":75,\"CourseName\":\"me420\"},{\"CourseScore\":69,\"CourseName\":\"cs431\"},{\"CourseScore\":71,\"CourseName\":\"me119\"},{\"CourseScore\":59,\"CourseName\":\"cs481\"},{\"CourseScore\":84,\"CourseName\":\"ec224\"},{\"CourseScore\":80,\"CourseName\":\"ci325\"},{\"CourseScore\":49,\"CourseName\":\"ec904\"},{\"CourseScore\":47,\"CourseName\":\"cs125\"},{\"CourseScore\":61,\"CourseName\":\"cs570\"}],\"RollNo\":202817271},{\"Name\":\"LOUIS\",\"CourseMarks\":[{\"CourseScore\":25,\"CourseName\":\"cs431\"},{\"CourseScore\":79,\"CourseName\":\"cs565\"},{\"CourseScore\":45,\"CourseName\":\"ec224\"},{\"CourseScore\":28,\"CourseName\":\"cs125\"}],\"RollNo\":202817272},{\"Name\":\"JONATHAN\",\"CourseMarks\":[{\"CourseScore\":33,\"CourseName\":\"me119\"},{\"CourseScore\":29,\"CourseName\":\"ci325\"},{\"CourseScore\":67,\"CourseName\":\"cs481\"},{\"CourseScore\":41,\"CourseName\":\"cs431\"},{\"CourseScore\":66,\"CourseName\":\"ec224\"},{\"CourseScore\":89,\"CourseName\":\"cs125\"},{\"CourseScore\":37,\"CourseName\":\"cs565\"}],\"RollNo\":202817273},{\"Name\":\"FRANCES\",\"CourseMarks\":[{\"CourseScore\":53,\"CourseName\":\"ec224\"},{\"CourseScore\":68,\"CourseName\":\"cs570\"},{\"CourseScore\":76,\"CourseName\":\"cs431\"},{\"CourseScore\":30,\"CourseName\":\"cs125\"},{\"CourseScore\":64,\"CourseName\":\"ci325\"},{\"CourseScore\":70,\"CourseName\":\"cs481\"},{\"CourseScore\":26,\"CourseName\":\"cs565\"}],\"RollNo\":202817274}]";
 //        System.out.println(content);
-        List<String> lines = Files.readAllLines(Paths.get("/Users/chandan5/sem7/DistributedSystems/json_googleProtobuf/input_sample"), StandardCharsets.UTF_8);
+        List<String> lines = Files.readAllLines(Paths.get("test/input_sample"), StandardCharsets.UTF_8);
         Student[] students = parseStringToStudents(lines);
         
-        Gson gson = new GsonBuilder().create();
-        
-        gson.toJson(students, System.out);
         long x = System.nanoTime();
-        System.out.println();
+        Gson gson = new GsonBuilder().create();
+//        gson.toJson(students, System.out);
+        String jsonResult = gson.toJson(students);
+//        System.out.println();
         long y = System.nanoTime();
         System.out.println(y-x);
+        x = System.nanoTime();
+        Result result = getResultProtoFromStudents(students);
+//        System.out.println(result.toString());
+        byte[] myByteArray = result.toByteArray();
+        y = System.nanoTime();
+        System.out.println(y-x);
+//        FileOutputStream fos = new FileOutputStream("test/outproto2.txt");
+//        fos.write(myByteArray);
+//        fos.close();
 //        gson.toJson("Hello", System.out);
 //        gson.toJson(123, System.out);
         //gson.toJson(persons, System.out);
