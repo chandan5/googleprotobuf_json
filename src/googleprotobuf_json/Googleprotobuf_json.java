2498d7418690e04774df33b97f44062d844de1ad
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2016-08-25 16:51:47 +0530

    Add metrics computation

--- src/googleprotobuf_json/Googleprotobuf_json.java
+++ src/googleprotobuf_json/Googleprotobuf_json.java
@@ -5,11 +5,9 @@
  */
 package googleprotobuf_json;
 
-import java.util.Scanner;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -19,7 +17,13 @@ import java.util.Arrays;
 import googleprotobuf_json.ResultProto.Result;
 import googleprotobuf_json.ResultProto.StudentMsg;
 import googleprotobuf_json.ResultProto.CourseMarks;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.NoSuchFileException;
 
 /**
  *
@@ -55,6 +59,40 @@ public class Googleprotobuf_json {
     /**
      * @param args the command line arguments
      */
+    private static void writeStudentsToFile(Student[] students, String outputFileName) throws Exception {
+        PrintWriter pw = new PrintWriter(new FileWriter(outputFileName));
+        
+        for(Student student: students) {
+            String outputString = "";
+            outputString += student.Name;
+            outputString += ",";
+            outputString += student.RollNo;
+            for(Course course: student.CourseMarks) {
+                outputString += ":";
+                outputString += course.CourseName;
+                outputString += ",";
+                outputString += course.CourseScore;
+            }
+            pw.println(outputString);
+        }
+        pw.close();
+    }
+    
+    private static void printStudents(Student[] students) {
+        for(Student student: students) {
+            String outputString = "";
+            outputString += student.Name;
+            outputString += ",";
+            outputString += student.RollNo;
+            for(Course course: student.CourseMarks) {
+                outputString += ":";
+                outputString += course.CourseName;
+                outputString += ",";
+                outputString += course.CourseScore;
+            }
+            System.out.println(outputString);
+        }
+    }
     
     private static Result getResultProtoFromStudents(Student[] students){
         Result.Builder result = Result.newBuilder();
@@ -81,31 +119,43 @@ public class Googleprotobuf_json {
             String name;
             int rollno;
             ArrayList<Course> coursesList = new ArrayList<Course>();
+            name = nameAndTokens[0];
+            String[] rollnoAndTokens = nameAndTokens[1].split(":",2);
+            rollno = Integer.parseInt(rollnoAndTokens[0]);
             
-//            if(nameAndTokens.length > 0) {
-                name = nameAndTokens[0];
-//            }
-//            if(nameAndTokens.length > 1) { 
-                String[] rollnoAndTokens = nameAndTokens[1].split(":",2);
-//                if(rollnoAndTokens.length > 0)
-                    rollno = Integer.parseInt(rollnoAndTokens[0]);
-//                System.out.print("rollnoAndTokens: ");
-//                System.out.println(Arrays.toString(rollnoAndTokens));
-                if(rollnoAndTokens.length > 1) {
-                    String[] courseStrings = rollnoAndTokens[1].split(":");
-//                    System.out.print("courseStrings: ");
-//                    System.out.println(Arrays.toString(courseStrings));
-                    for(String courseString: courseStrings) {
-//                        System.out.println(courseString);
-                        String[] courseStringArray = courseString.split(",");
-//                        System.out.println(Arrays.toString(courseStringArray));
-                        if(courseStringArray.length == 2) {
-                            Course course = new Course(courseStringArray[0], Integer.parseInt(courseStringArray[1]));
-                            coursesList.add(course);
-                        }
+            if(rollnoAndTokens.length > 1) {
+                String[] courseStrings = rollnoAndTokens[1].split(":");
+                for(String courseString: courseStrings) {
+                    String[] courseStringArray = courseString.split(",");
+                    if(courseStringArray.length == 2) {
+                        Course course = new Course(courseStringArray[0], Integer.parseInt(courseStringArray[1]));
+                        coursesList.add(course);
                     }
                 }
-//            }
+            }
+                
+            Course[] courses = coursesList.toArray(new Course[0]);
+            Student student = new Student(name, rollno, courses);
+            studentsList.add(student);
+        }
+        
+        Student[] students = studentsList.toArray(new Student[0]);
+        return students;
+    }
+    
+    private static Student[] getStudentsFromResult(Result result) {
+        ArrayList<Student> studentsList = new ArrayList<Student>();
+        
+        for(StudentMsg studentMsg: result.getStudentmsgList()) {
+            String name = studentMsg.getName();
+            int rollno = studentMsg.getRollNum();
+            ArrayList<Course> coursesList = new ArrayList<Course>();
+
+            for(CourseMarks courseMarks: studentMsg.getMarksList()) {
+                Course course = new Course(courseMarks.getName(), courseMarks.getScore());
+                coursesList.add(course);
+            }
+            
             Course[] courses = coursesList.toArray(new Course[0]);
             Student student = new Student(name, rollno, courses);
             studentsList.add(student);
@@ -113,42 +163,150 @@ public class Googleprotobuf_json {
         Student[] students = studentsList.toArray(new Student[0]);
         return students;
     }
+    
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        // TODO Ordered Dictionary
-//        try (Scanner scanner = new Scanner(System.in)) {
-//            String myString = scanner.next();
-//        }
+        // TODO Use options library http://stackoverflow.com/questions/367706/how-to-parse-command-line-arguments-in-java
+        if(args.length != 3) {
+            System.err.println("Format: -<s/d/t> -<p/j> file_name");
+            return;
+        }
+        System.err.println(Arrays.toString(args));
+        int serialise = -1;
+        int protobuf = -1;
+        String s = "-s";
+        String d = "-d";
+        String t = "-t";
+        String p = "-p";
+        String j = "-j";
+        if(s.equals(args[0])) {
+            serialise = 1;
+        }
+        else if(d.equals(args[0])) {
+            serialise = 0;
+        }
+        else if(t.equals(args[0])) {
+            serialise = 2;
+        }
+        if(p.equals(args[1])) {
+            protobuf = 1;
+        }
+        else if(j.equals(args[1])) {
+            protobuf = 0;
+        }
         
-//        String ss = "[{\"Name\":\"JANICE\",\"CourseMarks\":[{\"CourseScore\":54,\"CourseName\":\"cs125\"},{\"CourseScore\":73,\"CourseName\":\"me420\"},{\"CourseScore\":65,\"CourseName\":\"me119\"},{\"CourseScore\":44,\"CourseName\":\"cs431\"},{\"CourseScore\":67,\"CourseName\":\"cs481\"}],\"RollNo\":202817265},{\"Name\":\"JACK\",\"CourseMarks\":[{\"CourseScore\":42,\"CourseName\":\"ec904\"},{\"CourseScore\":69,\"CourseName\":\"me420\"},{\"CourseScore\":47,\"CourseName\":\"cs481\"},{\"CourseScore\":35,\"CourseName\":\"ec224\"},{\"CourseScore\":82,\"CourseName\":\"cs125\"},{\"CourseScore\":89,\"CourseName\":\"cs431\"},{\"CourseScore\":53,\"CourseName\":\"cs570\"},{\"CourseScore\":90,\"CourseName\":\"me119\"},{\"CourseScore\":40,\"CourseName\":\"cs565\"}],\"RollNo\":202817266},{\"Name\":\"WARREN\",\"CourseMarks\":[{\"CourseScore\":82,\"CourseName\":\"ec904\"}],\"RollNo\":202817267},{\"Name\":\"OSCAR\",\"CourseMarks\":[{\"CourseScore\":55,\"CourseName\":\"cs565\"},{\"CourseScore\":87,\"CourseName\":\"me119\"}],\"RollNo\":202817268},{\"Name\":\"BEATRICE\",\"CourseMarks\":[{\"CourseScore\":65,\"CourseName\":\"cs570\"},{\"CourseScore\":43,\"CourseName\":\"me119\"},{\"CourseScore\":57,\"CourseName\":\"ec224\"},{\"CourseScore\":36,\"CourseName\":\"ci325\"},{\"CourseScore\":33,\"CourseName\":\"cs125\"},{\"CourseScore\":74,\"CourseName\":\"cs431\"},{\"CourseScore\":81,\"CourseName\":\"me420\"},{\"CourseScore\":55,\"CourseName\":\"cs565\"}],\"RollNo\":202817269},{\"Name\":\"JEROME\",\"CourseMarks\":[{\"CourseScore\":32,\"CourseName\":\"ec904\"},{\"CourseScore\":59,\"CourseName\":\"ci325\"},{\"CourseScore\":25,\"CourseName\":\"cs570\"},{\"CourseScore\":58,\"CourseName\":\"cs431\"}],\"RollNo\":202817270},{\"Name\":\"KELLY\",\"CourseMarks\":[{\"CourseScore\":75,\"CourseName\":\"me420\"},{\"CourseScore\":69,\"CourseName\":\"cs431\"},{\"CourseScore\":71,\"CourseName\":\"me119\"},{\"CourseScore\":59,\"CourseName\":\"cs481\"},{\"CourseScore\":84,\"CourseName\":\"ec224\"},{\"CourseScore\":80,\"CourseName\":\"ci325\"},{\"CourseScore\":49,\"CourseName\":\"ec904\"},{\"CourseScore\":47,\"CourseName\":\"cs125\"},{\"CourseScore\":61,\"CourseName\":\"cs570\"}],\"RollNo\":202817271},{\"Name\":\"LOUIS\",\"CourseMarks\":[{\"CourseScore\":25,\"CourseName\":\"cs431\"},{\"CourseScore\":79,\"CourseName\":\"cs565\"},{\"CourseScore\":45,\"CourseName\":\"ec224\"},{\"CourseScore\":28,\"CourseName\":\"cs125\"}],\"RollNo\":202817272},{\"Name\":\"JONATHAN\",\"CourseMarks\":[{\"CourseScore\":33,\"CourseName\":\"me119\"},{\"CourseScore\":29,\"CourseName\":\"ci325\"},{\"CourseScore\":67,\"CourseName\":\"cs481\"},{\"CourseScore\":41,\"CourseName\":\"cs431\"},{\"CourseScore\":66,\"CourseName\":\"ec224\"},{\"CourseScore\":89,\"CourseName\":\"cs125\"},{\"CourseScore\":37,\"CourseName\":\"cs565\"}],\"RollNo\":202817273},{\"Name\":\"FRANCES\",\"CourseMarks\":[{\"CourseScore\":53,\"CourseName\":\"ec224\"},{\"CourseScore\":68,\"CourseName\":\"cs570\"},{\"CourseScore\":76,\"CourseName\":\"cs431\"},{\"CourseScore\":30,\"CourseName\":\"cs125\"},{\"CourseScore\":64,\"CourseName\":\"ci325\"},{\"CourseScore\":70,\"CourseName\":\"cs481\"},{\"CourseScore\":26,\"CourseName\":\"cs565\"}],\"RollNo\":202817274}]";
-//        System.out.println(content);
-        List<String> lines = Files.readAllLines(Paths.get("test/input_sample"), StandardCharsets.UTF_8);
-        Student[] students = parseStringToStudents(lines);
+        if(serialise == -1 || protobuf == -1) {
+            System.err.println("Format: -<s/d/t> -<p/j> file_name");
+            return;
+        }
         
-        long x = System.nanoTime();
-        Gson gson = new GsonBuilder().create();
-//        gson.toJson(students, System.out);
-        String jsonResult = gson.toJson(students);
-//        System.out.println();
-        long y = System.nanoTime();
-        System.out.println(y-x);
-        x = System.nanoTime();
-        Result result = getResultProtoFromStudents(students);
-//        System.out.println(result.toString());
-        byte[] myByteArray = result.toByteArray();
-        y = System.nanoTime();
-        System.out.println(y-x);
-//        FileOutputStream fos = new FileOutputStream("test/outproto2.txt");
-//        fos.write(myByteArray);
-//        fos.close();
-//        gson.toJson("Hello", System.out);
-//        gson.toJson(123, System.out);
-        //gson.toJson(persons, System.out);
-//        Student[] students = gson.fromJson(ss, Student[].class);
-//        Person[] ps = gson.fromJson("[{\"name\":\"Chandan\",\"age\":21},{\"name\":\"Karan\",\"age\":20}]", Person[].class);
-//        System.out.println(ps[0].name);
-//        System.out.println(students[0].CourseMarks[0].CourseName);
-//        System.out.println(students.toString());
+        try {
+            
+            String outputFileName = "";
+            String inputFileName = args[2];
+            if((serialise == 1) && (protobuf == 0)) {
+                outputFileName = "result.json";
+            }
+            else if((serialise == 1) && (protobuf == 1)) {
+                outputFileName = "result_protobuf";
+            }
+            else if((serialise == 0) && (protobuf == 0)) {
+                outputFileName = "output_json.txt";
+            }
+            else if((serialise == 0) && (protobuf == 1)) {
+                outputFileName = "output_protobuf.txt";
+            }
+            
+            Gson gson = new GsonBuilder().create();
+            if(serialise == 1) {
+                List<String> lines = Files.readAllLines(Paths.get(inputFileName), StandardCharsets.UTF_8);
+                Student[] students = parseStringToStudents(lines);
+                
+                if(protobuf == 0)     {
+                    String jsonStudents = gson.toJson(students);
+                    PrintWriter writer = new PrintWriter(outputFileName, "UTF-8");
+                    writer.print(jsonStudents);
+                    writer.close();
+//                     TODO Check why below code is not working
+//                     gson.toJson(students, new FileWriter(outputFileName));
+                }
+                else {
+                    Result result = getResultProtoFromStudents(students);
+                    byte[] myByteArray = result.toByteArray();
+                    FileOutputStream fos = new FileOutputStream(outputFileName);
+                    fos.write(myByteArray);
+                    fos.close();
+                }
+            }
+            else if(serialise == 0){
+                if(protobuf == 0) {
+                    Student[] students = gson.fromJson(new FileReader(inputFileName), Student[].class);
+                    writeStudentsToFile(students, outputFileName);
+                }
+                else {
+                    Result result = Result.parseFrom(new FileInputStream(inputFileName));
+                    Student[] students = getStudentsFromResult(result);
+                    writeStudentsToFile(students, outputFileName);
+                }
+            }
+            else if(serialise == 2) { // time measurements
+                File file =new File(inputFileName);
+		double inputKBs = 0;
+                double marshalledKbs = 0;
+                if(file.exists()){
+                    inputKBs = file.length()/1024;
+                }
+                else {
+                    System.err.println("Error: No such file exists. Kindly enter correct file path.");
+                    return;
+                }
+                List<String> lines = Files.readAllLines(Paths.get(inputFileName), StandardCharsets.UTF_8);
+                Student[] students = parseStringToStudents(lines);
+                long startSerialisation, stopSerialisation, startDeserialisation, stopDeserialisation;
+                if(protobuf == 0) {
+                    startSerialisation = System.nanoTime();
+                    String jsonStudents = gson.toJson(students);
+                    stopSerialisation = System.nanoTime();
+                    byte[] marshalledBytes = jsonStudents.getBytes("UTF-8");
+                    marshalledKbs = (marshalledBytes.length)/1024;
+                    startDeserialisation = System.nanoTime();
+                    gson.fromJson(jsonStudents, Student[].class);
+                    stopDeserialisation = System.nanoTime();
+                }
+                else {
+                    startSerialisation = System.nanoTime();
+                    Result result = getResultProtoFromStudents(students);
+                    byte[] marshalledBytes = result.toByteArray();
+                    stopSerialisation = System.nanoTime();
+                    marshalledKbs = (marshalledBytes.length)/1024;
+                    startDeserialisation = System.nanoTime();
+                    result = Result.parseFrom(marshalledBytes);
+                    getStudentsFromResult(result);
+                    stopDeserialisation = System.nanoTime();
+                }
+                long marshallingTime = (stopSerialisation-startSerialisation)/1000000;
+                long demarshallingTime = (stopDeserialisation-startDeserialisation)/1000000;
+                long marshallingRate = (long)(inputKBs*1000)/marshallingTime;
+                long demarshallingRate = (long)(marshalledKbs*1000)/demarshallingTime;
+                System.out.println("In put File Size: " + inputKBs + " kB");
+                System.out.println("Size after marshalling: " + marshalledKbs + " kB");
+                System.out.println("Time taken for marshalling: " + marshallingTime + " ms");
+                System.out.println("Time taken for demarshalling: " + demarshallingTime + " ms");
+                System.out.println("Rate of marshalling: " + marshallingRate + " kb/s");
+                System.out.println("Rate of demarshalling: " + demarshallingRate + " kb/s");
+                //            System.out.println((y-x)/1000000);
+            }
+
+        }
+        catch (NoSuchFileException e) {
+            System.err.println("Error: No such file exists. Kindly enter correct file path.");
+        }
+        catch (IOException e) {
+            System.err.println("Error : Reading/Writing Error. Please check permissons of file.");
+        }
+        catch (Exception e) {
+            System.err.println("Below mentioned error occured.");
+            System.err.println(e);
+        }
     }
     
 }
